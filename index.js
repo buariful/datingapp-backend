1454887a1389ef19e58ddf770197b26b00b236d5
buariful <arifulislam321054@gmail.com>
2022-09-16 10:22:33 +0600

    connecting

--- index.js
+++ index.js
@@ -19,10 +19,13 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+
 
 async function run() {
     try {
+        app.get('/ami', async (req, res) => {
+            res.send("ami")
+        })
         await client.connect();
         const database = client.db("datingapp").collection("users");
 
