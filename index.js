1c30aef36d05481d9e05a2197a21d3eedd7c6852
buariful <arifulislam321054@gmail.com>
2022-09-16 10:41:06 +0600

    kldjsl

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('dating app server is running smoothly')
+    res.send('dating app server is running smoothly 1')
 })
 
 
