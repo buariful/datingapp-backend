0b90de108494b2f79a2b6c663124393d87ab8b38
buariful <arifulislam321054@gmail.com>
2022-09-23 19:02:09 +0600

    jdsl

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('dating app server is running smoothly 1')
+    res.send('scissor app server is running smoothly 1')
 })
 
-
+// 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esxyjse.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,18 +24,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
 
-        app.get('/users', async (req, res) => {
-            const query = {};
-            const cursor = database.find(query);
-            const users = await cursor.toArray();
-            res.send("users")
-        })
         await client.connect();
-        const database = client.db("datingapp").collection("users");
+        const userCollection = client.db("scissorHand").collection("users");
 
-        app.get('/tomi', (req, res) => {
-            res.send("tomi")
+        app.post('/adduser', async (req, res) => {
+            const review = req.body;
+            const result = await userCollection.insertOne(review);
+            res.send(result);
         })
+
     }
     finally {
 
